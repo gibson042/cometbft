df1b4573e241853c033e74777ff0334d2337e4d2
lg <lauren@informal.systems>
2023-07-26 14:53:23 +0200

    Implement ResultBlockResults locally in grpc

--- rpc/grpc/client/block_results_service.go
+++ rpc/grpc/client/block_results_service.go
@@ -3,28 +3,39 @@ package client
 import (
 	"context"
 
+	abci "github.com/cometbft/cometbft/abci/types"
+	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+
 	"github.com/cosmos/gogoproto/grpc"
 
 	v1 "github.com/cometbft/cometbft/proto/tendermint/services/block_results/v1"
-	ctypes "github.com/cometbft/cometbft/rpc/core/types"
 )
+
+type ResultBlockResults struct {
+	Height                int64                     `json:"height"`
+	TxsResults            []*abci.ExecTxResult      `json:"txs_results"`
+	FinalizeBlockEvents   []abci.Event              `json:"finalize_block_events"`
+	ValidatorUpdates      []abci.ValidatorUpdate    `json:"validator_updates"`
+	ConsensusParamUpdates *cmtproto.ConsensusParams `json:"consensus_param_updates"`
+	AppHash               []byte                    `json:"app_hash"`
+}
 
 // BlockResultsServiceClient provides the block results of a given height (or latest if none provided).
 type BlockResultsServiceClient interface {
-	GetBlockResults(ctx context.Context, req v1.GetBlockResultsRequest) (*ctypes.ResultBlockResults, error)
+	GetBlockResults(ctx context.Context, req v1.GetBlockResultsRequest) (*ResultBlockResults, error)
 }
 
 type blockResultServiceClient struct {
 	client v1.BlockResultsServiceClient
 }
 
-func (b blockResultServiceClient) GetBlockResults(ctx context.Context, req v1.GetBlockResultsRequest) (*ctypes.ResultBlockResults, error) {
+func (b blockResultServiceClient) GetBlockResults(ctx context.Context, req v1.GetBlockResultsRequest) (*ResultBlockResults, error) {
 	res, err := b.client.GetBlockResults(ctx, &v1.GetBlockResultsRequest{Height: req.Height})
 	if err != nil {
-		return &ctypes.ResultBlockResults{}, err
+		return &ResultBlockResults{}, err
 	}
 
-	return &ctypes.ResultBlockResults{
+	return &ResultBlockResults{
 		Height:                res.Height,
 		TxsResults:            res.TxsResults,
 		FinalizeBlockEvents:   res.FinalizeBlockEvents,
@@ -47,6 +58,6 @@ func newDisabledBlockResultsServiceClient() BlockResultsServiceClient {
 }
 
 // GetBLockResults implements BlockResultsServiceClient
-func (*disabledBlockResultsServiceClient) GetBlockResults(_ context.Context, _ v1.GetBlockResultsRequest) (*ctypes.ResultBlockResults, error) {
+func (*disabledBlockResultsServiceClient) GetBlockResults(_ context.Context, _ v1.GetBlockResultsRequest) (*ResultBlockResults, error) {
 	panic("block results service client is disabled")
 }
